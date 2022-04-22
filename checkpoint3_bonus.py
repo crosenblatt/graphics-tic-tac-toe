f0b69efe97ea92d98127d7ccd3a1029847182cdc
crosenblatt <rosench13@gmail.com>
2022-04-22 07:45:36 -0700

    checkpoint 3 bonus

---
+++ checkpoint3_bonus.py
@@ -0,0 +1,15 @@
+won = False
+
+while not won:
+    guess = input("Enter a guess: ")
+
+    length = len(guess)
+    if length < 5:
+        print("Your guess was too few letters. Your word was " + str(length) + " letters in length. Try again and guess a 5 letter word.")
+    elif length > 5:
+        print("Your guess was too many letters. Your word was " + str(length) + " letters in length. Try again and guess a 5 letter word.")
+    elif guess == "proxy":
+        print("Congrats you won!")
+        won = True
+    else:
+        print("That's not right. Guess again.")
